c17ac2d69d10e2f985c1b4257d79298206d77ab0
Curtis Brandt <curtis@cpan.org>
2015-03-29 15:52:43 -0700

    allow user to sort posts and fetch controversial

--- lib/Mojo/Snoo/Base.pm
+++ lib/Mojo/Snoo/Base.pm
@@ -2,13 +2,18 @@ package Mojo::Snoo::Base;
 use Mojo::Base -base;
 
 use Mojo::UserAgent;
+use Mojo::URL;
 use Mojo::Util ();
 
 has base_url => 'http://www.reddit.com';
 
 sub _get {
-    my ($self, $path) = @_;
-    my $url = join('', $self->base_url, $path, '.json');
+    my ( $self, $path, %params ) = @_;
+
+    my $url = Mojo::URL->new( $self->base_url );
+    $url->path("$path.json");
+    $url->query(%params) if %params;
+
     Mojo::UserAgent->new->get($url)->res->json;
 }
 
@@ -19,7 +24,7 @@ sub _create_object {
 
 sub _monkey_patch {
     my ($self, $class, $patch) = @_;
-    
+
     Mojo::Util::monkey_patch(
         $class,
         map {
--- lib/Mojo/Snoo/Subreddit.pm
+++ lib/Mojo/Snoo/Subreddit.pm
@@ -3,12 +3,19 @@ use Mojo::Base 'Mojo::Snoo::Base';
 
 use Mojo::Collection;
 use Mojo::Snoo::Thing;
-use Mojo::Util qw(monkey_patch);
 
 has 'name';
-has about  => \&_build_about;
-has things => \&_build_things;
-has mods   => \&_build_mods;
+has about               => \&_build_about;
+has mods                => \&_build_mods;
+has things              => \&_build_things;
+has things_hot          => \&_build_things;
+has things_new          => \&_build_things_new;
+has things_rising       => \&_build_things_rising;
+has things_contro       => \&_build_things_contro;
+has things_contro_today => \&_build_things_contro;
+has things_contro_week  => \&_build_things_contro_week;
+has things_contro_month => \&_build_things_contro_month;
+has things_contro_all   => \&_build_things_contro_all;
 
 sub _build_mods {
     my $self = shift;
@@ -27,18 +34,18 @@ sub _build_mods {
 
 sub _build_about {
     my $self = shift;
-    my $json = $self->_get('/r/'.$self->name.'/about');
+    my $json = $self->_get( '/r/' . $self->name . '/about' );
 
     my $pkg = 'Mojo::Snoo::Subreddit::About::' . $self->name;
     $self->_monkey_patch( $pkg, $json->{data} );
 }
 
-# fetch things
-# create new thing objects
-# TODO pass params
-sub _build_things {
-    my $self = shift;
-    my $json = $self->_get('/r/'.$self->name);
+sub _get_things {
+    my ( $self, $sort, $time ) = @_;
+    my $path = '/r/' . $self->name;
+    $path .= "/$sort" if $sort;
+
+    my $json = $self->_get( $path, ( $time ? ( t => $time ) : () ) );
 
     my @children =
       map { $_->{kind} eq 't3' ? $_->{data} : () }    #
@@ -47,6 +54,14 @@ sub _build_things {
     my @things = map Mojo::Snoo::Thing->new(%$_), @children;
     Mojo::Collection->new(@things);
 }
+
+sub _build_things              { shift->_get_things() }
+sub _build_things_new          { shift->_get_things('new') }
+sub _build_things_rising       { shift->_get_things('rising') }
+sub _build_things_contro       { shift->_get_things('controversial') }
+sub _build_things_contro_week  { shift->_get_things( 'controversial', 'week' ) }
+sub _build_things_contro_month { shift->_get_things( 'controversial', 'week' ) }
+sub _build_things_contro_all   { shift->_get_things( 'controversial', 'week' ) }
 
 1;
 
