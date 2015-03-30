a2966eefc22093627cdec43f38763cc866f862ab
Curtis Brandt <curtis@cpan.org>
2015-03-30 01:17:00 -0700

    include contro/top yearly posts

--- lib/Mojo/Snoo/Subreddit.pm
+++ lib/Mojo/Snoo/Subreddit.pm
@@ -66,10 +66,12 @@ sub things_rising       { shift->_get_things( shift, 'rising' ) }
 sub things_contro       { shift->_get_things( shift, 'controversial' ) }
 sub things_contro_week  { shift->_get_things( shift, 'controversial', 'week' ) }
 sub things_contro_month { shift->_get_things( shift, 'controversial', 'month' ) }
+sub things_contro_year  { shift->_get_things( shift, 'controversial', 'year' ) }
 sub things_contro_all   { shift->_get_things( shift, 'controversial', 'all' ) }
 sub things_top          { shift->_get_things( shift, 'top' ) }
 sub things_top_week     { shift->_get_things( shift, 'top',           'week' ) }
 sub things_top_month    { shift->_get_things( shift, 'top',           'month' ) }
+sub things_top_year     { shift->_get_things( shift, 'top',           'year' ) }
 sub things_top_all      { shift->_get_things( shift, 'top',           'all' ) }
 
 1;
